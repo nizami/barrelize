c1438817e49e190c5a8e836f8c632a274a657567
nizami <nizami.dev@gmail.com>
2025-05-10 19:19:49 +0300

    Update schema defaults and integrate DEFAULT_CONFIG values into schema generation

--- src/schema/generate-schema.ts
+++ src/schema/generate-schema.ts
@@ -1,6 +1,12 @@
 import {json} from 'typia';
-import {Config} from '../config/config';
+import {Config, DEFAULT_CONFIG} from '../config/config.js';
 
 const schema: any = json.schema<Config>().components.schemas?.['Config'];
+
+const {properties} = schema.properties.directories.items;
+
+for (const [key, value] of Object.entries(DEFAULT_CONFIG)) {
+  properties[key]['default'] = value;
+}
 
 console.log(JSON.stringify({$schema: 'http://json-schema.org/draft-07/schema', ...schema}, null, 2));
