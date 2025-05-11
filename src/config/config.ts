edeedcd6edca1771963c018bd1a4321ce6b25cc3
nizami <nizami.dev@gmail.com>
2025-05-11 15:17:50 +0300

    Improve Config type add additional formatting properties

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,24 +1,35 @@
 import {createValidateEquals} from 'typia';
 
-export type Config = {
-  $schema?: string;
-  directories: {
-    path?: string;
-    include?: string[];
-    exclude?: string[];
-    order?: string[];
-    indexFilePath?: string;
-    replace?: {find: string; replacement: string}[];
-  }[];
+export type DirectoryConfig = {
+  path?: string;
+  include?: string[];
+  exclude?: string[];
+  order?: string[];
+  indexFilePath?: string;
+  replace?: {find: string; replacement: string}[];
+  singleQuote?: boolean;
+  semi?: boolean;
+  insertFinalNewline?: boolean;
 };
 
-export const DEFAULT_CONFIG: Required<Config['directories'][0]> = {
+export type Config = {
+  $schema?: string;
+  singleQuote?: boolean;
+  semi?: boolean;
+  insertFinalNewline?: boolean;
+  directories: DirectoryConfig[];
+};
+
+export const DEFAULT_CONFIG: Required<DirectoryConfig> = {
   path: '',
   include: ['**/*.ts'],
   exclude: [],
   order: [],
   indexFilePath: 'index.ts',
   replace: [{find: '\\.ts$', replacement: ''}],
+  singleQuote: true,
+  semi: true,
+  insertFinalNewline: true,
 };
 
 export const validateConfig = createValidateEquals<Config>();
