fd17c6f4db561eb9845b99ac1026a7874c151d34
nizami <nizami.dev@gmail.com>
2025-05-11 17:06:14 +0300

    Refactor generate-barrels and index files to improve path handling and exports

--- src/generate/generate-barrels.ts
+++ src/generate/generate-barrels.ts
@@ -1,7 +1,15 @@
-import {colorize, Config, DEFAULT_CONFIG, logError, logWarning, TerminalColor} from '#lib';
-import {glob} from 'glob';
+import {
+  colorize,
+  Config,
+  DEFAULT_CONFIG,
+  generateBarrel,
+  indexFileExportedPaths,
+  logWarning,
+  pathsDifferences,
+  TerminalColor,
+} from '#lib';
 import {existsSync} from 'node:fs';
-import {readFile, writeFile} from 'node:fs/promises';
+import {writeFile} from 'node:fs/promises';
 import {dirname, join, resolve} from 'node:path';
 
 export async function generateBarrels(rootPath: string, configPath: string, config: Config): Promise<void> {
@@ -18,7 +26,7 @@ export async function generateBarrels(rootPath: string, configPath: string, conf
       continue;
     }
 
-    const files = await generateBarrel(rootPath, directoryConfig);
+    const paths = await generateBarrel(rootPath, directoryConfig);
     const quote = directoryConfig.singleQuote ?? config.singleQuote ?? DEFAULT_CONFIG.singleQuote ? "'" : '"';
     const semiIfNeeded = directoryConfig.semi ?? config.semi ?? DEFAULT_CONFIG.semi ? ';' : '';
     const newLineIfNeeded =
@@ -27,123 +35,50 @@ export async function generateBarrels(rootPath: string, configPath: string, conf
         : '';
 
     const formatExportLine = (path: string) => `export * from ${quote}./${path}${quote}${semiIfNeeded}`;
-    const content = files.map(formatExportLine).join('\n') + newLineIfNeeded;
+    const content = paths.map(formatExportLine).join('\n') + newLineIfNeeded;
 
     if (existsSync(indexFileAbsolutePath)) {
-      const oldPaths = await getIndexExportedPaths(indexFileAbsolutePath);
+      const oldPaths = await indexFileExportedPaths(indexFileAbsolutePath);
 
       await writeFile(indexFileAbsolutePath, content);
+      const {insertions, deletions} = pathsDifferences(oldPaths, paths);
+
+      const exportedText = `${paths.length} file${plural(paths)} exported`;
+
+      const insertionsText =
+        insertions.length > 0 ? `, ${insertions.length} insertion${plural(insertions)}` : '';
+
+      const deletionsText = deletions.length > 0 ? `, ${deletions.length} deletion${plural(deletions)}` : '';
 
       console.log(
         colorize('UPDATE', TerminalColor.GREEN),
         colorize(indexFileRelativePath, TerminalColor.CYAN),
-        colorize(`exports ${files.length} file${files.length > 1 ? 's' : ''}`, TerminalColor.GRAY),
+        colorize(`${exportedText}${insertionsText}${deletionsText}`, TerminalColor.GRAY),
       );
 
-      printDifferences(oldPaths, files);
+      printDifferences(insertions, deletions);
     } else {
       await writeFile(indexFileAbsolutePath, content);
 
       console.log(
         colorize('CREATE', TerminalColor.GREEN),
         colorize(indexFileRelativePath, TerminalColor.CYAN),
-        colorize(`exports ${files.length} file${files.length > 1 ? 's' : ''}`, TerminalColor.GRAY),
+        colorize(`exports ${paths.length} file${paths.length > 1 ? 's' : ''}`, TerminalColor.GRAY),
       );
     }
   }
 }
 
-async function getIndexExportedPaths(indexFilePath: string): Promise<string[]> {
-  const indexFileContent = await readFile(indexFilePath);
-  const matches = indexFileContent.toString().match(/'.+'/g);
-
-  return matches?.map((x) => x) ?? [];
-}
-
-function printDifferences(oldPaths: string[], newPaths: string[]): void {
-  for (const path of oldPaths) {
-    if (!newPaths.includes(path)) {
-      console.log(colorize(`  - ${path}`, TerminalColor.RED));
-    }
+function printDifferences(insertions: string[], deletions: string[]): void {
+  for (const path of insertions) {
+    console.log(colorize(`  + ${path}`, TerminalColor.GREEN));
   }
 
-  for (const path of newPaths) {
-    if (!oldPaths.includes(path)) {
-      console.log(colorize(`  + ${path}`, TerminalColor.GREEN));
-    }
+  for (const path of deletions) {
+    console.log(colorize(`  - ${path}`, TerminalColor.RED));
   }
 }
 
-async function generateBarrel(
-  rootPath: string,
-  directoryConfig: Config['directories'][0],
-): Promise<string[]> {
-  const ignore = [...(directoryConfig.exclude ?? DEFAULT_CONFIG.exclude), '**/index.ts', '**/index.js'];
-  const cwd = resolve(rootPath, directoryConfig.path ?? DEFAULT_CONFIG.path);
-
-  let files = await glob(directoryConfig.include ?? DEFAULT_CONFIG.include, {
-    cwd,
-    ignore,
-    nodir: true,
-    includeChildMatches: true,
-  });
-  files = files.map((file) => file.replace(/\\/g, '/'));
-  files = handleOrder(directoryConfig, files);
-  files = handlePathReplacement(directoryConfig, files);
-
-  return files;
+function plural(array: unknown[]): string {
+  return array.length > 1 ? 's' : '';
 }
-
-function handlePathReplacement(config: Config['directories'][0], files: string[]): string[] {
-  const replaces = config.replace ?? DEFAULT_CONFIG.replace;
-
-  for (let i = 0; i < replaces.length; i++) {
-    const {find, replacement} = replaces[i];
-
-    try {
-      const regexp = new RegExp(find);
-      files = files.map((file) => file.replace(regexp, replacement));
-    } catch (error) {
-      if (error instanceof SyntaxError) {
-        logError(
-          error.message.replace(
-            'Invalid regular expression:',
-            `Invalid regular expression number ${i + 1} in config.`,
-          ),
-        );
-      } else {
-        logWarning(`Invalid regular expression number ${i + 1}`);
-      }
-    }
-  }
-
-  return files;
-}
-
-function handleOrder(config: Config['directories'][0], files: string[]): string[] {
-  files = files.sort((a, b) => a.localeCompare(b));
-  const orders = config.order ?? DEFAULT_CONFIG.order;
-
-  if (!orders.length) {
-    return files;
-  }
-
-  return files.sort((a, b) => sortPathWeight(config.order!, b) - sortPathWeight(config.order!, a));
-}
-
-function sortPathWeight(order: string[], path: string): number {
-  const orderIndex = order.findIndex((x) => path.startsWith(x));
-
-  if (orderIndex >= 0) {
-    return (order.length - orderIndex) * 100 - path.split('/').length;
-  }
-
-  return -path.split('/').length;
-}
-
-// async function main() {
-//   await Promise.all(directories.map((directory) => createBarrel(directory)));
-//   console.log('\x1b[32m%s\x1b[0m', 'Barrels Done!');
-// }
-
-// main().catch(console.error);
