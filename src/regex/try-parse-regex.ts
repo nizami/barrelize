de5203bc9d7466562b04677ab7cf9f62dae56de0
nizami <nizami.dev@gmail.com>
2025-05-13 19:21:05 +0300

    Add tryParseRegex function and corresponding tests

---
+++ src/regex/try-parse-regex.ts
@@ -0,0 +1,37 @@
+import {logWarning} from '#lib';
+
+export function tryParseRegex(s: string): RegExp | null {
+  if (!s.startsWith('/')) {
+    return null;
+  }
+
+  const lastSlashIndex = s.lastIndexOf('/');
+
+  if (lastSlashIndex <= 0) {
+    return null;
+  }
+
+  const flags = s.slice(lastSlashIndex + 1);
+
+  if (flags && !/^[dgimsuvy]*$/.test(flags)) {
+    return null;
+  }
+
+  const pattern = s.slice(1, lastSlashIndex);
+
+  try {
+    return new RegExp(pattern, flags);
+  } catch (error) {
+    if (error instanceof Error) {
+      logWarning(error.message);
+    } else {
+      console.error(error);
+    }
+
+    return null;
+  }
+}
+
+export function rsTest(rs: RegExp | string, s: string): boolean {
+  return typeof rs === 'string' ? rs === s : rs.test(s);
+}
